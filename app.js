90f8f65a612e9ed3f22c74c3657e4b6b04981724
czx <chenzhixiang@yit.com>
2019-09-30 18:16:03 +0800

    init

---
+++ app.js
@@ -0,0 +1,12 @@
+const inquirer = require('inquirer');
+
+class ApiTemplate {
+    constructor() {
+        this.init();
+    }
+    init() {
+        console.error("init...")
+    }
+}
+
+new ApiTemplate();
