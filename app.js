9aecdf52ed88304fa562b0a7a2a8aca62b8528ce
czx <chenzhixiang@yit.com>
2019-09-30 18:25:33 +0800

    inquirer

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
+const config = require("./config");
 
 class ApiTemplate {
-    constructor() {
-        this.init();
-    }
-    init() {
-        console.error("init...")
-    }
+  constructor() {
+    this.init();
+  }
+  async init() {
+    const argv = await this.getArgv();
+    console.log(argv, config);
+  }
+  getArgv() {
+    return inquirer.prompt([
+      // {
+      //   type: "input",
+      //   name: "api",
+      //   message: "请输入接口名:"
+      // },
+      {
+        type: "list",
+        name: "safe",
+        message: "请选择安全等级",
+        choices: ["Anonym", "User"]
+      }
+    ]);
+  }
 }
 
 new ApiTemplate();
